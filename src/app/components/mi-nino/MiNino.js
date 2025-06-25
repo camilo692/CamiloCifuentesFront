c917d9868daf69b454312e82b56db28647cc3d6f
David Steven Ochoa Muñoz <dsochoam@gmail.com>
2025-06-25 18:40:06 -0500

    Replace footer menu items with Next.js Link components for improved

--- src/app/components/mi-nino/MiNino.js
+++ src/app/components/mi-nino/MiNino.js
@@ -1,8 +1,12 @@
 "use client";
 import Image from 'next/image';
 import './MiNino.css';
+import { usePathname } from 'next/navigation';
 
 const MiNino = () => {
+  const pathname = usePathname();
+  const showSocialIcons = pathname && pathname.includes('/nosotros');
+
   return (
     <div className="mi-nino-container">
       <div className="mi-nino-content">
@@ -14,6 +18,14 @@ const MiNino = () => {
           priority
         />
       </div>
+      {showSocialIcons && (
+        <div className="iconos-sociales1">
+          <a href="#"><img src="/tiktok.png" alt="TikTok" /></a>
+          <a href="#"><img src="/instagram.png" alt="Instagram" /></a>
+          <a href="#"><img src="/facebook.png" alt="Facebook" /></a>
+          <a href="#"><img src="/youtube.png" alt="YouTube" /></a>
+        </div>
+      )}
     </div>
   );
 };
